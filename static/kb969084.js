4a5bd154254550b2dde290f56615df325b908aa4
Yonggang Luo <luoyonggang@gmail.com>
2020-09-13 03:43:44 +0800

    Fixes kb969084 link lost

---
+++ static/kb969084.js
@@ -0,0 +1,273 @@
+const kb969084_xp = [
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-nor_0b874a0d8025127b32193d1525b45eeea665fcaa.exe",
+    digest: "C4dKDYAlEnsyGT0VJbRe7qZl/Ko=",
+    architectures: "X86",
+    languages: "no",
+    longLanguages: "Norwegian",
+    fileName:
+      "windowsxp-kb969084-x86-nor_0b874a0d8025127b32193d1525b45eeea665fcaa.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-dan_1a43a84c96e599d4a92b476fa8f5ae00a7dc0208.exe",
+    digest: "GkOoTJblmdSpK0dvqPWuAKfcAgg=",
+    architectures: "X86",
+    languages: "da",
+    longLanguages: "Danish",
+    fileName:
+      "windowsxp-kb969084-x86-dan_1a43a84c96e599d4a92b476fa8f5ae00a7dc0208.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-cht_c033c5e6bbcdc9f69ef484ecb5d314d5278efa0d.exe",
+    digest: "wDPF5rvNyfae9ITstdMU1SeO+g0=",
+    architectures: "X86",
+    languages: "zh-tw",
+    longLanguages: "Chinese (Traditional)",
+    fileName:
+      "windowsxp-kb969084-x86-cht_c033c5e6bbcdc9f69ef484ecb5d314d5278efa0d.exe",
+    defaultFileNameLength: 121,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-sve_6e19e45eb0676a21d5480114d2d5057eb51271ce.exe",
+    digest: "bhnkXrBnaiHVSAEU0tUFfrUScc4=",
+    architectures: "X86",
+    languages: "sv",
+    longLanguages: "Swedish",
+    fileName:
+      "windowsxp-kb969084-x86-sve_6e19e45eb0676a21d5480114d2d5057eb51271ce.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-rus_659a7bcea0a75bbd458a646a5321ce7725594123.exe",
+    digest: "ZZp7zqCnW71FimRqUyHOdyVZQSM=",
+    architectures: "X86",
+    languages: "ru",
+    longLanguages: "Russian",
+    fileName:
+      "windowsxp-kb969084-x86-rus_659a7bcea0a75bbd458a646a5321ce7725594123.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-ita_bdff0f715be5e700e62c9fe29ad680f18db36557.exe",
+    digest: "vf8PcVvl5wDmLJ/imtaA8Y2zZVc=",
+    architectures: "X86",
+    languages: "it",
+    longLanguages: "Italian",
+    fileName:
+      "windowsxp-kb969084-x86-ita_bdff0f715be5e700e62c9fe29ad680f18db36557.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-enu_153587172bf060ddc9d2c966f1eaaf4f47926f28.exe",
+    digest: "FTWHFyvwYN3J0slm8eqvT0eSbyg=",
+    architectures: "X86",
+    languages: "en",
+    longLanguages: "English",
+    fileName:
+      "windowsxp-kb969084-x86-enu_153587172bf060ddc9d2c966f1eaaf4f47926f28.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-ptg_81a1ca70594377d5af3056304d5e40c63be1f2a7.exe",
+    digest: "gaHKcFlDd9WvMFYwTV5Axjvh8qc=",
+    architectures: "X86",
+    languages: "pt",
+    longLanguages: "Portuguese (Portugal)",
+    fileName:
+      "windowsxp-kb969084-x86-ptg_81a1ca70594377d5af3056304d5e40c63be1f2a7.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-kor_3888d8adb5e9419eaadf5f492df92b4fc705fb24.exe",
+    digest: "OIjYrbXpQZ6q319JLfkrT8cF+yQ=",
+    architectures: "X86",
+    languages: "ko",
+    longLanguages: "Korean",
+    fileName:
+      "windowsxp-kb969084-x86-kor_3888d8adb5e9419eaadf5f492df92b4fc705fb24.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-ell_c6b260c0dc95f5ffd09175305329db7b99261e9a.exe",
+    digest: "xrJgwNyV9f/QkXUwUynbe5kmHpo=",
+    architectures: "X86",
+    languages: "el",
+    longLanguages: "Greek",
+    fileName:
+      "windowsxp-kb969084-x86-ell_c6b260c0dc95f5ffd09175305329db7b99261e9a.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-deu_ed7039095a5f388a200fe2dacd4ab2d2bba51eac.exe",
+    digest: "7XA5CVpfOIogD+LazUqy0rulHqw=",
+    architectures: "X86",
+    languages: "de",
+    longLanguages: "German",
+    fileName:
+      "windowsxp-kb969084-x86-deu_ed7039095a5f388a200fe2dacd4ab2d2bba51eac.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-trk_56abcc6825c9d206445b4254410bf6dc05571504.exe",
+    digest: "VqvMaCXJ0gZEW0JUQQv23AVXFQQ=",
+    architectures: "X86",
+    languages: "tr",
+    longLanguages: "Turkish",
+    fileName:
+      "windowsxp-kb969084-x86-trk_56abcc6825c9d206445b4254410bf6dc05571504.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-plk_d573a09974ee74bfdd675f992a80194ed68a7eda.exe",
+    digest: "1XOgmXTudL/dZ1+ZKoAZTtaKfto=",
+    architectures: "X86",
+    languages: "pl",
+    longLanguages: "Polish",
+    fileName:
+      "windowsxp-kb969084-x86-plk_d573a09974ee74bfdd675f992a80194ed68a7eda.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-fin_81ba4dad45421866c1952356a34f58b06a91c89f.exe",
+    digest: "gbpNrUVCGGbBlSNWo09YsGqRyJ8=",
+    architectures: "X86",
+    languages: "fi",
+    longLanguages: "Finnish",
+    fileName:
+      "windowsxp-kb969084-x86-fin_81ba4dad45421866c1952356a34f58b06a91c89f.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-ara_7a020613d7a17f224195fc3529b28f2a4868e064.exe",
+    digest: "egIGE9ehfyJBlfw1KbKPKkho4GQ=",
+    architectures: "X86",
+    languages: "ar",
+    longLanguages: "Arabic",
+    fileName:
+      "windowsxp-kb969084-x86-ara_7a020613d7a17f224195fc3529b28f2a4868e064.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-ptb_4bad921582fcd643ed650bb87dcf53292087b64a.exe",
+    digest: "S62SFYL81kPtZQu4fc9TKSCHtko=",
+    architectures: "X86",
+    languages: "pt-br",
+    longLanguages: "Portuguese (Brazil)",
+    fileName:
+      "windowsxp-kb969084-x86-ptb_4bad921582fcd643ed650bb87dcf53292087b64a.exe",
+    defaultFileNameLength: 121,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-hun_8ef4950f8440a00d226c41ba3bbc305ad234cc8b.exe",
+    digest: "jvSVD4RAoA0ibEG6O7wwWtI0zIs=",
+    architectures: "X86",
+    languages: "hu",
+    longLanguages: "Hungarian",
+    fileName:
+      "windowsxp-kb969084-x86-hun_8ef4950f8440a00d226c41ba3bbc305ad234cc8b.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-nld_b79e248ccb98d9abd798d518546eaa93e85b758f.exe",
+    digest: "t54kjMuY2avXmNUYVG6qk+hbdY8=",
+    architectures: "X86",
+    languages: "nl",
+    longLanguages: "Dutch",
+    fileName:
+      "windowsxp-kb969084-x86-nld_b79e248ccb98d9abd798d518546eaa93e85b758f.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-heb_36b682c711a36698bd0191dc7d11b3448da9b9ab.exe",
+    digest: "NraCxxGjZpi9AZHcfRGzRI2puas=",
+    architectures: "X86",
+    languages: "he",
+    longLanguages: "Hebrew",
+    fileName:
+      "windowsxp-kb969084-x86-heb_36b682c711a36698bd0191dc7d11b3448da9b9ab.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-esn_1a6d67eb39caca54e6231dc9f5da288597db4aa1.exe",
+    digest: "Gm1n6znKylTmIx3J9doohZfbSqE=",
+    architectures: "X86",
+    languages: "es",
+    longLanguages: "Spanish",
+    fileName:
+      "windowsxp-kb969084-x86-esn_1a6d67eb39caca54e6231dc9f5da288597db4aa1.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-csy_cea0835514c90263b6f7c9501be76493106ae965.exe",
+    digest: "zqCDVRTJAmO298lQG+dkkxBq6WU=",
+    architectures: "X86",
+    languages: "cs",
+    longLanguages: "Czech",
+    fileName:
+      "windowsxp-kb969084-x86-csy_cea0835514c90263b6f7c9501be76493106ae965.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-jpn_2b865a79dd12191f7f354d8edadd4a0dcf559f7d.exe",
+    digest: "K4Zaed0SGR9/NU2O2t1KDc9Vn30=",
+    architectures: "X86",
+    languages: "ja",
+    longLanguages: "Japanese",
+    fileName:
+      "windowsxp-kb969084-x86-jpn_2b865a79dd12191f7f354d8edadd4a0dcf559f7d.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-fra_ce1e6c4b68f040f6cfafdc2547698a2b3849f5e5.exe",
+    digest: "zh5sS2jwQPbPr9wlR2mKKzhJ9eU=",
+    architectures: "X86",
+    languages: "fr",
+    longLanguages: "French",
+    fileName:
+      "windowsxp-kb969084-x86-fra_ce1e6c4b68f040f6cfafdc2547698a2b3849f5e5.exe",
+    defaultFileNameLength: 118,
+  },
+  {
+    url:
+      "http://download.windowsupdate.com/msdownload/update/software/updt/2009/11/windowsxp-kb969084-x86-chs_bbe85fd63f2cae2ef9cd6428f8648e51e9b9cf9f.exe",
+    digest: "u+hf1j8sri75zWQo+GSOUem5z58=",
+    architectures: "X86",
+    languages: "zh-cn",
+    longLanguages: "Chinese (Simplified)",
+    fileName:
+      "windowsxp-kb969084-x86-chs_bbe85fd63f2cae2ef9cd6428f8648e51e9b9cf9f.exe",
+    defaultFileNameLength: 121,
+  },
+];
+
+
+module.exports = {
+    xp: kb969084_xp
+}
+
+console.log(kb969084_xp.length)
